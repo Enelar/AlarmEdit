6f2d1033add49809bdd8b50bec356ff2d29e1761
Kirill Berezin <enelar@develop-project.ru>
2014-04-07 16:28:12 +0400

    Blah blah

--- api/alarm_prio.php
+++ api/alarm_prio.php
@@ -2,7 +2,7 @@
 
 class alarm_prio extends api
 {
-  protected function Reserve()
+  protected function Reserve( $level = 1 )
   {
     return [
       "design" => "table",
@@ -10,7 +10,7 @@ class alarm_prio extends api
       "data" => 
       [
         "types" => LoadModule('api', 'alarm_prio')->Types(),
-        "tags" => LoadModule('api', 'alarm_prio')->Tags()
+        "tags" => LoadModule('api', 'alarm_prio')->Tags($level)
       ]
     ];
   }
@@ -39,30 +39,40 @@ class alarm_prio extends api
     
   }
 
-  protected function Tags()
+  protected function Tags( $level = 1 )
   {
-    $res = db::Query("SELECT id, CAST(name as text) as name, CAST(description as text) as description FROM [dbo].[TAG] ORDER BY TAG.name ASC");
+    $res = db::Query("
+      SELECT id, CAST(name as text) as name, CAST(description as text) as description
+      FROM [dbo].[TAG]
+      WHERE is_analog = 1
+      ORDER BY TAG.name ASC");
     $ret = [];
     foreach ($res as $row)
     {
-      $row['values'] = $this->TagValues($row['id']);
+      $row['values'] = $this->TagValues($row['id'], $level);
+      if (!count($row['values']))
+        continue;
       array_push($ret, $row);
     }
     return ["data" => $ret];
   }
 
-  public function TagValues( $id )
+  public function TagValues( $id, $level = 1 )
   {
-    $res = db::Query("SELECT id, type, priority FROM [dbo].[ALARM] WHERE id_tag=$id");
+    $res = db::Query("SELECT id, type, priority FROM [dbo].[ALARM] WHERE id_tag=$id AND BASE_LEVEL = $level");
     $ret = [];
-    foreach ($res as $row)
-      $ret[$row['type']] = $row;
+    if (count($res))
+      foreach ($res as $row)
+        $ret[$row['type']] = $row;
     return $ret;
   }
   
   protected function Types()
   {
-    $ret = db::Query("SELECT type as name FROM [dbo].[ALARM] GROUP BY type");
+    //$ret = db::Query("SELECT type as name FROM [dbo].[ALARM] GROUP BY type");
+    $ret = [];
+    foreach (["HH", "HI", "LO", "LL", "NR", "IOP", "IOP-", "ANS+", "ANS-", "ALM"] as $t)
+      array_push($ret, ['name' => $t]);
     return ["data" => $ret];
   }
   
